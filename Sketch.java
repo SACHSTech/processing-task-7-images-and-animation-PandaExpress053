8d3b037e78a06aeba9ef4824e7c085c2a65f51c9
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-04-23 20:32:13 -0400

    modified:   Sketch.java 	new file:   background.png 	new file:   cartoon-bee.png

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,40 @@
 import processing.core.PApplet;
-
+import processing.core.PImage;
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  // Initialize global variables
+	PImage imgBackground;
+  PImage imgBee;
+
+  float fltRectX;
+  float fltRectY;
+
+  float fltRectSpeedX = 2;
+  float fltRectSpeedY = 1;
+
+  float fltBeeX = 100;
+  float fltBeeY = 100;
+
+  float fltBeeSpeedX = 3;
+  float fltBeeSpeedY;
+
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    
+    // set size of screen
+    size(610, 510);
+
+    // set size of images
+    imgBackground = loadImage("background.png");
+    imgBee = loadImage("cartoon-bee.png");
+    imgBee.resize(imgBee.width / 6,imgBee.height / 6);
+
+    // stating initial starting position
+    fltRectX = width / 2;
+    fltRectY = height / 2;
+
+    fltBeeX = width / 2;
+    fltBeeY = height / 2; 
   }
 
   /** 
@@ -17,20 +43,45 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // setting background
+	  image(imgBackground, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // rectangle linear movement
+    rect(fltRectX, fltRectY, 30, 15);
+    fltRectX += fltRectSpeedX;
+    fltRectY += fltRectSpeedY;
+
+    // collision detection for rectangle
+    if (fltRectX < 0 || fltRectX > width - 30) {
+      fltRectSpeedX *= -1;
+    }
   
-  // define other methods down here.
+    if (fltRectY < 0  || fltRectY > height - 15) {
+      fltRectSpeedY *= -1;
+    }
+    
+    // bee parabolic movement
+    image(imgBee, fltBeeX, fltBeeY);
+    fltBeeSpeedY = 5 * (float) (Math.sin((fltBeeX) * (float)(Math.PI / 180)));
+    fltBeeX += fltBeeSpeedX;
+
+    // keeps bee in Y-position threshold, so parabolic movement doesn't push bee beyond screen size
+    if (fltBeeY + fltBeeSpeedY < 150  || fltBeeY + fltBeeSpeedY > height - 150) {}
+    else{
+      fltBeeY += fltBeeSpeedY;
+    }
+    // collision detection of screen width constraints
+    if (fltBeeX < -40 || fltBeeX > width - 110) {
+      fltBeeSpeedX *= -1;
+      fltBeeSpeedY *= -1;
+    }
+    
+  }
 }
